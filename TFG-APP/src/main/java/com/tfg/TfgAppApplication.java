9d62a29387939b2647ab6275a085606d202e10b5
pablomz34 <martinezmunozpablo@gmail.com>
2022-11-21 19:06:26 +0100

    Initial commit

---
+++ TFG-APP/src/main/java/com/tfg/TfgAppApplication.java
@@ -0,0 +1,13 @@
+package com.tfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgAppApplication.class, args);
+	}
+
+}
---
+++ TFG-APP/src/test/java/com/tfg/TfgAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
