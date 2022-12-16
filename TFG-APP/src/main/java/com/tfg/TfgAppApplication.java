96e42807b2b25b7a83ab9d6fac3e6a67f4ca6a05
pablomz34 <martinezmunozpablo@gmail.com>
2022-12-16 21:36:55 +0100

    conectado front a index.html

--- TFG-APP/src/main/java/com/tfg/TfgAppApplication.java
+++ TFG-APP/src/main/java/com/tfg/TfgAppApplication.java
@@ -1,13 +1,27 @@
 package com.tfg;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.tfg.entities.Medicos;
+import com.tfg.repositories.MedicosRepository;
+
 @SpringBootApplication
-public class TfgAppApplication {
+public class TfgAppApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TfgAppApplication.class, args);
+	}
+	
+	@Autowired
+	private MedicosRepository medRep;
+
+	@Override
+	public void run(String... args) throws Exception {
+		//Medicos medicos = Medicos.builder();
+		
 	}
 
 }
