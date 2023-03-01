845a25d60d3d1b6e1336f1678fff7da63ee871dd
Cristinita26 <60843242+cristina26ioana@users.noreply.github.com>
2023-03-01 17:31:25 +0100

    Python con variables

--- TFG-APP/src/main/java/com/tfg/TfgAppApplication.java
+++ TFG-APP/src/main/java/com/tfg/TfgAppApplication.java
@@ -3,9 +3,12 @@ package com.tfg;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class TfgAppApplication {
@@ -14,9 +17,9 @@ public class TfgAppApplication {
 		SpringApplication.run(TfgAppApplication.class, args);
 
 		try {
+			Integer x = 1, y = 2;
 			// Se lanza el ejecutable.
-			String ruta = "cmd /c " + System.getProperty("user.dir") + "\\src\\main\\resources\\python\\HolaMundo.py";
-			
+			String ruta = "cmd /c " + System.getProperty("user.dir") + "\\src\\main\\resources\\python\\HolaMundo.py " + x + " " + y;
 			Process p = Runtime.getRuntime().exec(ruta);
 
 			// Se obtiene el stream de salida del programa
--- TFG-APP/src/main/resources/python/HolaMundo.py
+++ TFG-APP/src/main/resources/python/HolaMundo.py
@@ -1,1 +1,8 @@
-print("Hola, mundo")
+import sys;
+
+def funcion(num1,num2): 
+    c = int(num1)+int(num2);
+    return int(c);
+    
+
+print(funcion(sys.argv[1],sys.argv[2]))
