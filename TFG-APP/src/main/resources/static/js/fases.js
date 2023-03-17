63a9af1dbf95db13ba1197457bc1901038d033e7
asmolano <61146725+asmolano@users.noreply.github.com>
2023-03-17 10:45:51 +0100

    arreglo merge de fases

--- TFG-APP/src/main/resources/static/js/fases.js
+++ TFG-APP/src/main/resources/static/js/fases.js
@@ -2,8 +2,10 @@ Vue.component('fase1', {
 	data: function() {
 		return {
 			nombreFase: 'Fase1',
+			algoritmos: ['K', 'M', 'H', 'A'],
+			algoritmo: '',
 			nClusters: '',
-			csvFile: '',
+			csv: '',
 			nClustersFile: '',
 			imagenCreada: false,
 			imagenUrl: ''
@@ -28,10 +30,10 @@ Vue.component('fase1', {
 			})*/
 		},
 
-		asyncGetNClusters() {
+		asyncGetNClusters: function() {
 			const THIZ = this;
 			const formData = new FormData();
-			$('#cargando').show();
+
 			formData.append('max_clusters', this.nClusters);
 			formData.append('file', this.$refs.csvFile.files[0]);
 
@@ -41,48 +43,53 @@ Vue.component('fase1', {
 			})
 				.then(res => res.arrayBuffer())
 				.then(image_bytes => {
-
+					
 					const byteArray = new Uint8Array(image_bytes);
-					const blob = new Blob([byteArray], { type: 'image/png' });
-					const url = URL.createObjectURL(blob);
-					THIZ.imagenCreada = true;
-					THIZ.imagenUrl = url;
-					$('#cargando').hide();
-				})
+				    const blob = new Blob([byteArray], { type: 'image/png' });
+				    const url = URL.createObjectURL(blob);
+				    THIZ.imagenCreada = true;
+				    THIZ.imagenUrl = url;
+				})	
 				.catch(err => console.log(err));
+				
+				
 		}
 	},
 
 
 	template: `
-	<div class="container col-md-12">
-		<span>
-			<div id="cargando" style="position:fixed; display:none; width: 100%; height: 100%; margin:0; padding:0; top:0; left:0; background:rgba(255,255,255,0.75);">
-        		<img id="cargando" src="/images/cargando.gif" style="top:50%; left:50%; position: fixed; transform: translate(-50%, -50%);"/>
-   			 </div>
-		</span>
+	<div>
+		<p>{{nombreFase}}</p>
 		
-		<div class="col-md-6 p-2 m-3" style="border:1px solid black; border-radius:10px; padding:20px">
 			<form @submit.prevent="asyncGetNClusters">				
-			 
-				<div class="form-group col-md-4 pb-4">
-					<label class="form-label" for="nClusters">Numero de clusters</label>
-				    <input type="number" min=0 max=8 class="form-control" v-model="nClusters" id="nClusters">
-				</div>
-				
-				<div class="form-group col-md-6 pb-4">
-					<input type="file" accept=".csv" class="form-control-file" id="csv" ref="csvFile">
-				</div>
-				
-				<button type="submit" class="btn btn-primary">Ejecutar</button>
-			</form>
-		</div>
-		<div v-if="imagenCreada" class="col-md-6 p-2 m-3">
-			<img id="imagenFase1" v-bind:src="imagenUrl"/>
-		</div>
+		 	<!--<div class="form-group col-md-6 pb-4">
+				<label class="form-label" for="algoritmo">Tipo de algoritmo</label>
+				<select v-model="algoritmo" class="form-control" id="algoritmo">
+					<option value="" disabled selected>Selecciona un tipo de algoritmo</option>
+					<option v-for="a in algoritmos">{{a}}</option>
+				</select>
+			</div>-->
+			
+			<div class="form-group col-md-6 pb-4">
+				<label class="form-label" for="nClusters">Numero de clusters</label>
+			    <input type="number" min=0 max=8 class="form-control" v-model="nClusters" id="nClusters">
+			</div>
+			
+			<div class="form-group col-md-6 pb-4">
+				<input type="file" accept=".csv" class="form-control-file" id="csv" ref="csvFile">
+			</div>
+			
+			<button type="submit" class="btn btn-primary">Ejecutar</button>
+			
+			<div v-if="imagenCreada">
+				<img id="imagenFase1" v-bind:src="imagenUrl"/>
+			</div>
+			
+		</form>
 		
 	</div>
 	`
+
 });
 
 Vue.component('fase2', {
@@ -181,17 +188,71 @@ Vue.component('fase2', {
 Vue.component('fase3', {
 	data: function() {
 		return {
-			nombreFase: 'Fase3',
+			lista: [],
+			headers: [{ header: "Metric", pos: 0 }, { header: "Tss_value", pos: 1 }, { header: "Total_wc", pos: 2 }, { header: "Total_bc", pos: 3 }],
+			datosCargados: false
 		}
 	},
 
+	methods: {
+		asyncGetVarianceMetrics: function() {
+			const THIZ = this;
+			const formData = new FormData();
 
+			formData.append('file', this.$refs.csvFile.files[0]);
+
+			fetch(window.location.origin + "/fases/getVarianceMetrics", {
+				method: "POST",
+				body: formData
+			})
+				.then(response => response.json())
+				.then(data =>{
+					for(i=0, j=1; j<data.length; i++,j++) THIZ.lista[i] = data[j]
+					THIZ.datosCargados=true;
+					//THIZ.lista = data;
+				})
+				.catch(error => console.error(error));
+				
+				
+		
+		}
+	},
 
 	template: `
 	<div>
-		<p>{{nombreFase}}</p>
+		
+		<form @submit.prevent="asyncGetVarianceMetrics">				
+			
+			<div class="form-group col-md-6 pb-4">
+				<input type="file" accept=".csv" class="form-control-file" id="csv" ref="csvFile">
+			</div>
+			
+			<button type="submit" class="btn btn-primary">Ejecutar</button>
+			
+				
+		</form>
+		
+		<table v-if="datosCargados" class="table table-bordered table-hover">
+			<thead class="table-dark">
+				<tr>
+					<th v-for="head in headers"> 
+					{{head.header}}
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="i in lista">
+					<td>{{i.metric}}</td>
+					<td>{{i.tss_value}}</td>
+					<td>{{i.total_wc}}</td>
+					<td>{{i.total_bc}}</td>
+				</tr>
+			</tbody>
+		</table>
+			
+		
+		
 	</div>
-	
 	`
 
 });
@@ -265,6 +326,7 @@ Vue.component('fase5', {
 	`
 
 });
+
 
 new Vue({
 	el: "#fases",
